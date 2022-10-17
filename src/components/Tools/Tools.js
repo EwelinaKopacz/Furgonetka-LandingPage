43adce184249019d82982e43511ecb5d7e42ea0c
Ewelina <ewelina@ekopacz.pl>
2022-10-17 22:55:39 +0200

    removed comments from Tools component

--- src/components/Tools/Tools.js
+++ src/components/Tools/Tools.js
@@ -3,12 +3,6 @@ import './Tools.css';
 import ToolsCard from '../ToolsCard/ToolsCard';
 import { toolsIconArray1, toolsIconArray2 } from '../../db/toolsIcons';
 
-// import toolsPhoto1 from '../../assets/tools-photo1.svg';
-// import toolsPhoto2 from '../../assets/tools-photo2.svg';
-// import toolsPhoto3 from '../../assets/tools-photo3.svg';
-// import toolsPhoto4 from '../../assets/tools-photo4.svg';
-// import toolsPhoto5 from '../../assets/tools-photo5.svg';
-
 const Tools = () => {
     const renderToolsCard =(array)=>{
         return array.map(item => <ToolsCard item={item} key={item.id}/>)
@@ -17,14 +11,9 @@ const Tools = () => {
         <section className='tools'>
             <div className='tools__container tools'>
                 {renderToolsCard(toolsIconArray1)}
-                {/* <ToolsCard photo={toolsPhoto1} text='Śledzenie przesyłek'/>
-                <ToolsCard photo={toolsPhoto2} text='Książka adresowa'/>
-                <ToolsCard photo={toolsPhoto3} text='Szablony przesyłek'/> */}
             </div>
             <div className='tools__container tools'>
                 {renderToolsCard(toolsIconArray2)}
-                {/* <ToolsCard photo={toolsPhoto4} text='Inne dane na etykiecie'/>
-                <ToolsCard photo={toolsPhoto5} text='Zamówienie paczki bez zlecania podjazdu kuriera'/> */}
             </div>
          </section>
     )
