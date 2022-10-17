cc22145ac502d3335cd8c733d2eee93d1bf677d2
Ewelina <ewelina@ekopacz.pl>
2022-10-17 18:33:03 +0200

    added layout Header and Main to the App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,14 @@
 import React from 'react';
 import './App.css';
-import NavBar from "../NavBar/NavBar";
-import Hero from "../Hero/Hero";
-import Intro from "../Intro/Intro";
-import Offer from '../Offer/Offer';
-import Features from '../Features/Features';
-import Brands from '../Brands/Brands';
-import Advert from '../Advert/Adver';
-import Form from "../Form/Form";
+import Header from '../../layout/Header/Header';
+import Main from '../../layout/Main/Main';
 
 
 function App() {
   return (
     <div className='wrapper'>
-      <NavBar/>
-      <Hero/>
-      <Intro
-        header={<><span className='intro__header--italic'>Wybierz tanie i szybkie</span> przesyłki krajowe</>}
-        text='Wyceniaj i wysyłaj tanie paczki krajowe bez podpisywania umowy i określania minimum logistycznego. U nas płacisz tylko za nadane przesyłki.
-        Nie pobieramy abonamentu za prowadzenie konta czy korzystanie z dostępnych narzędzi.'
-        className="intro__container--margin48"
-        />
-      <Offer/>
-      <Features/>
-      <Brands/>
-      <Advert/>
-      <Form/>
+      <Header/>
+      <Main/>
     </div>
   );
 }
---
+++ src/layout/Main/Main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Hero from "../../components/Hero/Hero";
+import Intro from "../../components/Intro/Intro";
+import Offer from '../../components/Offer/Offer';
+import Features from '../../components/Features/Features';
+import Brands from '../../components/Brands/Brands';
+import Advert from '../../components/Advert/Adver';
+import Form from "../../components/Form/Form";
+
+const Main=()=> {
+    return (
+        <>
+            <Hero/>
+            <Intro
+            header={<><span className='intro__header--italic'>Wybierz tanie i szybkie</span> przesyłki krajowe</>}
+            text='Wyceniaj i wysyłaj tanie paczki krajowe bez podpisywania umowy i określania minimum logistycznego. U nas płacisz tylko za nadane przesyłki.
+            Nie pobieramy abonamentu za prowadzenie konta czy korzystanie z dostępnych narzędzi.'
+            className="intro__container--margin48"
+            />
+            <Offer/>
+            <Features/>
+            <Brands/>
+            <Advert/>
+            <Form/>
+      </>
+    )
+}
+export default Main;
