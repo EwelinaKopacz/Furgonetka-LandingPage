c2ad5eada5ccbbb3576f654ddf700406802a7d49
Ewelina <ewelina@ekopacz.pl>
2022-10-31 08:25:29 +0100

    test bg path

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Button = ({text, className='', onClick}) => {
+const Button = ({text, className=''}) => {
     return (
         <div className='btn btn__container'>
-            <button className={className} onClick={onClick}>
+            <button className={className}>
                 <p className='btn__text'>{text}</p>
             </button>
         </div>
