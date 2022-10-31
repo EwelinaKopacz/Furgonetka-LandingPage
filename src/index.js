63ef50b99615f0c251d86b4dce7ca4936153656b
Ewelina <ewelina@ekopacz.pl>
2022-10-31 08:40:43 +0100

    test bg path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
-import "./styles.css";
 
 const root = ReactDOM.createRoot(document.querySelector('.root'));
 root.render(
